46b35717f30c870354ae01c558d80e2d0a3224dc
wzw <1187190846@qq.com>
2023-02-04 14:57:17 +0800

    second

--- springcloud-api/src/main/java/com/wzw/springcloud/pojo/Dept.java
+++ springcloud-api/src/main/java/com/wzw/springcloud/pojo/Dept.java
@@ -17,6 +17,7 @@ public class Dept implements Serializable {
     private String dname;
     //这个数据存在哪个数据库的字段  微服务  一个服务对应一个数据库， 同一个信息可能存在不同的数据库
     private String db_source;
+    private String test;
     public Dept(String dname){
         this.dname = dname;
     }
